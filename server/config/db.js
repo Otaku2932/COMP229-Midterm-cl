e1b658c2d4bc76b6bf901cc1114db8a81d2b1a3f
Otaku2932 <c_lapis4@hotmail.com>
2020-10-30 20:23:15 -0400

    MongoAtlas Connection Finished

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://comp229cl:4UUGTNOJlRwFE7Fv@comp229cl.obmbw.mongodb.net/books229?retryWrites=true&w=majority"
 };
